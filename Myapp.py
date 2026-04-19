aced3021b11e5cd4a752e71559c54f9b854fc395
Faustine-code <claessensfaustine29@gmail.com>
2026-04-19 21:57:37 +0200

    added Break line

--- Myapp.py
+++ Myapp.py
@@ -4,5 +4,5 @@ import streamlit as st
 #st.container() is function call that returns an object. And we'll be able to include some buttons inside
 with st.container():
     st.subheader("Sales Manager")
-    st.write({"Location:": "Zurich, SUI",
-              "\nDuration:": "3 months"})
+    st.write("Location:": "Zurich, SUI")
+    st.write("Duration:": "3 months")
