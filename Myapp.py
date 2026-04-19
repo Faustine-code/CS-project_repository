4eebb3e7fbda4b6e2c82d86e01b5fde150ca5173
Faustine-code <claessensfaustine29@gmail.com>
2026-04-19 21:54:18 +0200

    Sales manager

--- Myapp.py
+++ Myapp.py
@@ -2,5 +2,7 @@ import streamlit as st
 
 #with tells that everything that will be intented will have to be inside the box
 #st.container() is function call that returns an object. And we'll be able to include some buttons inside
-with st.container(border= True):
-    st.write("Sales Manager - 3 months")
+with st.container():
+    st.subheader("Sales Manager")
+    st.write({"Location:": "Zurich, SUI",
+              "\nDuration:": "3 months"})
