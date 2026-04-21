a810fe57ab8f4217bfb5c3e4b502523191da8957
Faustine-code <claessensfaustine29@gmail.com>
2026-04-21 20:06:34 +0200

    added colours

--- Myapp.py
+++ Myapp.py
@@ -1,9 +1,22 @@
 import streamlit as st
 
-#with tells that everything that will be intented will have to be inside the box
-#st.container() is function call that returns an object. And we'll be able to include some buttons inside
-with st.container(border=True):
+    st.markdown("""
+<style>
+.custom-card {
+    background-color: #87CEEB;
+    padding: 15px;
+    border-radius: 10px;
+    border: 1px solid #ccc;
+}
+</style>
+""", unsafe_allow_html=True)
+
+with st.container():
+    st.markdown('<div class="custom-card">', unsafe_allow_html=True)
+    
     st.subheader("Sales Manager")
     st.write("Location:", "Zurich, SUI")
     st.write("Duration:", "3 months")
     st.write("Tags:", "Sales, Revenues")
+    
+    st.markdown('</div>', unsafe_allow_html=True)
